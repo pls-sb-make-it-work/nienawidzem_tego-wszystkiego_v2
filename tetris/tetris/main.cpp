15d284aee995ab3d57a230e85fd216150e60f592
pls-sb-make-it-work <46862514+pls-sb-make-it-work@users.noreply.github.com>
2019-01-20 12:58:21 +0100

    Add files via upload

---
+++ tetris/tetris/main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+#include <time.h>
+#include "game.h"
+
+int main()
+
+{
+	game dupa;
+
+	dupa.Run();
+	dupa.Update();
+	dupa.Draw();
+	dupa.Check();
+	dupa.Move();
+	dupa.Rotate();
+	dupa.Tick();
+
+	return 0;
+}
